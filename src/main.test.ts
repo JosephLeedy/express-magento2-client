094b2c087f1f7595d6717a532600321f08966205
Joseph Leedy <joseph@leedy.us>
2023-12-22 18:16:35 -0600

    Fix server not shutting down after tests are run

--- src/main.test.ts
+++ src/main.test.ts
@@ -1,8 +1,12 @@
 import request from 'supertest'
 import type {Response} from 'supertest'
-import app from './main'
+import app, {server} from './main'
 
 describe('Index Route', (): void => {
+    afterAll((): void => {
+        server.close()
+    })
+
     it('sends a message with a status of OK', (done: jest.DoneCallback): void => {
         request(app)
             .get('/')
--- src/main.ts
+++ src/main.ts
@@ -1,12 +1,14 @@
+import { Server } from 'node:http'
 import express, {Request, Response} from 'express'
 
 const app = express()
+export let server: Server
 
 app.get('/', (_request: Request, response: Response): void => {
     response.send('Happy birthday! 🎂');
 })
 
-app.listen(3000, (): void => {
+server = app.listen(3000, (): void => {
     console.log('Server is listening on port 3000...\n')
 })
 
