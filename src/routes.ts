91608eea07e0929aa54e75fd92db39290e6bca2e
Joseph Leedy <joseph@leedy.us>
2024-01-02 16:35:43 -0600

    Load routes relative to current path

--- src/routes.ts
+++ src/routes.ts
@@ -1,4 +1,5 @@
 import path from 'node:path'
+import {fileURLToPath} from 'node:url'
 import {Router} from 'express'
 import {glob} from 'glob'
 import homeRoutes from './routes/home.js'
@@ -19,7 +20,16 @@ routes.use('/', homeRoutes)
 
 routeFiles.forEach(async (routeFile: string): Promise<void> => {
     const fileName: string = path.basename(routeFile.replace(/\.ts$/, '.js'))
-    const routerImport: {default: Router} = await import('./routes/' + fileName)
+    let routePath = path.relative(
+        path.dirname(fileURLToPath(import.meta.url)), path.dirname(routeFile) + '/' + fileName
+    )
+    let routerImport: {default: Router}
+
+    if (!routePath.startsWith('.')) {
+        routePath = './' + routePath
+    }
+
+    routerImport = await import(routePath)
 
     routes.use('/' + path.basename(fileName, '.js'), routerImport.default)
 })
